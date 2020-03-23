38d22098cda5673aaf1375c6ea1b54b15af531c5
Rahul Parmar <rahulparmar.n@gmail.com>
2020-03-24 01:42:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,71 @@
-const core = require('@actions/core');
+const core = require(`@actions/core`);
 const fs = require("fs");
 
 try {
 
-    core.debug(`Started performing action...`);
-
     const PROJECT_DIRECTORY_PATH = `${process.env.PROJECT_DIRECTORY_PATH}`;
+    const PROJECT_CSPROJ_FILE_PATH = `${process.env.PROJECT_DIRECTORY_PATH}/${process.env.PROJECT_DIRECTORY_PATH}.csproj`;
+    const IIS_SERVER_COMPUTER_NAME = `${process.env.IIS_SERVER_COMPUTER_NAME}`
+    const IIS_WEBSITE_NAME = `${process.env.IIS_WEBSITE_NAME}`
+    const IIS_SERVER_USERNAME = `${process.env.IIS_SERVER_USERNAME}`
+    const IIS_SERVER_PASSWORD = `${process.env.IIS_SERVER_PASSWORD}`
+    const RECYCLE_APP_NAME = `${process.env.RECYCLE_APP_NAME}`
+    const WEBSITE_CONTENT_PATH = `${process.env.WEBSITE_CONTENT_PATH}`
 
     // Make sure path to source code directory is available
-    if (!fs.existsSync(PROJECT_DIRECTORY_PATH)) {
-        core.setFailed(`Source directory does not exist: ${PROJECT_DIRECTORY_PATH}`);
-    } else {
+    if (fs.existsSync(PROJECT_DIRECTORY_PATH)) {
         core.debug(`Source dirctory: ${PROJECT_DIRECTORY_PATH}`);
+    } else {
+        core.setFailed(`Source directory does not exist: ${PROJECT_DIRECTORY_PATH}`);
     }
 
-    const PROJECT_CSPROJ_FILE_PATH = `${process.env.PROJECT_DIRECTORY_PATH}/${process.env.PROJECT_DIRECTORY_PATH}.csproj`;
+    if (fs.existsSync(PROJECT_CSPROJ_FILE_PATH)) {
+        core.debug(`Default csproj file: ${PROJECT_CSPROJ_FILE_PATH}`);
 
-    if (!fs.existsSync(PROJECT_CSPROJ_FILE_PATH)) {
-        core.setFailed(`Source directory does not exist: ${PROJECT_CSPROJ_FILE_PATH}`);
     } else {
-        core.debug(`Source dirctory: ${PROJECT_CSPROJ_FILE_PATH}`);
+        core.setFailed(`Default csproj file does not exist: ${PROJECT_CSPROJ_FILE_PATH}`);
+    }
+
+    if (process.env.IIS_SERVER_COMPUTER_NAME) {
+        core.debug('IIS_SERVER_COMPUTER_NAME is set');
+    }
+    else {
+        core.setFailed(`IIS_SERVER_COMPUTER_NAME is not set: ${IIS_SERVER_COMPUTER_NAME}`);
+    }
+
+    if (process.env.IIS_WEBSITE_NAME) {
+        core.debug('IIS_WEBSITE_NAME is set');
+    }
+    else {
+        core.setFailed(`IIS_WEBSITE_NAME is not set: ${IIS_WEBSITE_NAME}`);
+    }
+
+    if (process.env.IIS_SERVER_USERNAME) {
+        core.debug('IIS_SERVER_USERNAME is set');
+    }
+    else {
+        core.setFailed(`IIS_SERVER_USERNAME is not set: ${IIS_SERVER_USERNAME}`);
+    }
+
+    if (process.env.IIS_SERVER_PASSWORD) {
+        core.debug('IIS_SERVER_PASSWORD is set');
+    }
+    else {
+        core.setFailed(`IIS_SERVER_PASSWORD is not set: ${IIS_SERVER_PASSWORD}`);
+    }
+
+    if (process.env.RECYCLE_APP_NAME) {
+        core.debug('RECYCLE_APP_NAME is set');
+    }
+    else {
+        core.setFailed(`RECYCLE_APP_NAME is not set: ${RECYCLE_APP_NAME}`);
+    }
+
+    if (process.env.WEBSITE_CONTENT_PATH) {
+        core.debug('WEBSITE_CONTENT_PATH is set');
+    }
+    else {
+        core.setFailed(`WEBSITE_CONTENT_PATH is not set: ${WEBSITE_CONTENT_PATH}`);
     }
 
 } catch (error) {
